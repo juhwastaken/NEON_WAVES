6147cbee1713f2c6a57c1039779cd26aae8e3f72
danteco <dantepereirabraga@gmail.com>
2025-04-23 18:36:33 -0300

    DEU CERTO

--- JOGO/Assets/Menu/PauseMenu.cs
+++ JOGO/Assets/Menu/PauseMenu.cs
@@ -85,6 +85,7 @@ public class PauseManager : MonoBehaviour
     {
         Time.timeScale = 1f;
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        
     }
 
     public void OpenOptions()
